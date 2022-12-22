0411181534a782f24f558ed0f65417a9cb56c094
alimostafa1221 <alimostafa156890@gmail.com>
2022-12-22 23:37:20 +0200

    update integer to float

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ let sumNumber = document.getElementById("sumNumber")
 sumNumber.addEventListener("click", function () {
     let firstNum = num1.value
     let secondNum = num2.value
-    const sum = parseInt(firstNum) + parseInt(secondNum)
+    const sum = parseFloat(firstNum) + parseFloat(secondNum)
     console.log(`${sum}`);
     let rowNumber = `
     <div class="result" >
@@ -12,5 +12,10 @@ sumNumber.addEventListener("click", function () {
     </div>
     `
     document.getElementById("resultNumber").innerHTML = rowNumber
+    clearInp()
 })
+function clearInp(){
+    num1.value = "";
+    num2.value = "";
+}
 
